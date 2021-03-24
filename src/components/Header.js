cb0d13e2c82fe1e767a562a725424c4ef18536da
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2021-03-24 20:03:25 +0200

    Final

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<div className='container'>
+				<h1 className='title'>
+				
+					React <span>Movie</span> Search
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
